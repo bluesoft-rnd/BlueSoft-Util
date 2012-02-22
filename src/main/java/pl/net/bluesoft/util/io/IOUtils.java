4cfaf010a7163b7b079e5c31c91fb9aa40784e21
zmalinowski <zmalinowski@bluesoft.net.pl>
2012-02-22 17:26:49 +0100

    fix in IOUtils, but class marked as deprecated

--- src/main/java/pl/net/bluesoft/util/io/IOUtils.java
+++ src/main/java/pl/net/bluesoft/util/io/IOUtils.java
@@ -5,13 +5,15 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
+ * Usage of this class is discouraged. Please use e.g. Apache Commons IO instead.
  * @author tlipski@bluesoft.net.pl
  */
+@Deprecated
 public class IOUtils {
 	public static byte[] slurp(InputStream is) throws IOException {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		int c=0;
-		while ((c = is.read()) > 0) {
+		while ((c = is.read()) >= 0) {
 			baos.write(c);
 		}
 		return baos.toByteArray();
